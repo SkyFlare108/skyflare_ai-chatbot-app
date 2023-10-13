d36d9c7982b17fb6befce4122c0fabb85fda21f7
Hari Yerramsetti <63610353+SkyFlare108@users.noreply.github.com>
2023-10-13 19:48:13 -0400

    Updated Working AI.py Solution

--- AI.py
+++ AI.py
@@ -1,27 +1,86 @@
 import os
+import spacy
+
+nlp = spacy.load("en_core_web_sm")
 from pathlib import Path
 
 current_directory = os.path.dirname(os.path.abspath(__file__))
-
 print("\n*Welcome to SkyFlare ChatApp*\n")
 user = input("Account Username: ")
-
+check = True
+programCheck = True
 file_name = "{}.txt".format(user)
 file_path = os.path.join(current_directory, file_name)
 
 if os.path.exists(file_path):
     with open(file_name, "a") as file:
-        print("Account Exists")
-        first_line = next(open("{}.txt".format(user)))
-        passWord = input("Account Password: ")
-        
-        # while passWord != first_line:
-        #     passWord = input("Try Again: ")
-
-        print(">>>Welcome to your Data<<<")
+        with open(file_path, 'r') as file:
+            first_lineCheck = file.readline()
+            while check:
+                passWord = input("Account Password: ")
+                first_line = next(open("{}.txt".format(user)))
+                if first_lineCheck.strip() == passWord.strip():
+                    print(">>>Welcome to your Data<<<")
+                    check = False
+                else:
+                    print("Wrong Password")
 else:
     with open(file_path, "w") as file:
         print("Account Doesn't exist\nCreating...")
         passWord = input("Create account Password: ")
         file.write("{}\n".format(passWord))
+
+def load_questions_and_answers(file_path):
+    qa_dict = {}
+    try:
+        with open(file_path, 'r') as file:
+            for i, line in enumerate(file):
+                if i == 0:
+                    continue
+                else:
+                    question, answer = line.strip().split('|')
+                    qa_dict[question] = answer
+        return qa_dict
+    except FileNotFoundError:
+        return {}
+
+def save_question_and_answer(file_path, question, answer):
+    with open(file_path, 'a') as file:
+        file.write(f"{question}|{answer}\n")
+
+def get_most_similar_question(user_question, qa_dict):
+    max_similarity = 0
+    most_similar_question = None
+    for question in qa_dict:
+        similarity = nlp(user_question).similarity(nlp(question))
+        if similarity > max_similarity:
+            max_similarity = similarity
+            most_similar_question = question
+    return most_similar_question, max_similarity
+
+def chatbot(file_path):
+    qa_dict = load_questions_and_answers(file_path)
+
+    while True:
+        user_question = input("You: ")
+        if user_question.lower() == 'exit':
+            print("Chatbot: Goodbye!")
+            break
+
+        if user_question in qa_dict:
+            print(f"Chatbot: {qa_dict[user_question]}")
+        else:
+            most_similar_question, similarity = get_most_similar_question(user_question, qa_dict)
+            if similarity > 0.7:  # You can adjust this threshold as needed
+                print(f"Chatbot: Did you mean '{most_similar_question}'? {qa_dict[most_similar_question]}")
+            else:
+                print("Chatbot: I don't know the answer to that. Please tell me.")
+                new_answer = input("You (provide the answer): ")
+                qa_dict[user_question] = new_answer
+                save_question_and_answer(file_path, user_question, new_answer)
+                print("Chatbot: Thanks, I've learned something new!")
+
+if __name__ == "__main__":
+    print("Chatbot: Hello! You can ask me anything. Type 'exit' to end the conversation.")
+    chatbot(file_name)
 
